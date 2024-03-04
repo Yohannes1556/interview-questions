38bc7e90ab5ef421c988439c86f6e61be90f6ef1
Yohannes1556 <johnaddisu1556@gmail.com>
2024-03-05 00:09:49 +0300

    star rating question

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Accordian from './components/Accordian/Accordian';
 import RandomColor from './components/RandomColor';
+import  StarRating from './components/StarRating/StarRating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
 
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
